12c10288b4f53b790d3861c1f703e3ca8bbef3c9
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-01-28 15:56:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function fetchBooks(){
 return fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
-  .then(json => renderBooks());
+  .then(json => renderBooks(json));
 } 
 function renderBooks(json) {
   const main = document.querySelector('main')
